b491814ed7f7c3fe8b14cfa679afcfdbe96dfe5e
Spiros Makris <makris.spiros@gmail.com>
2020-01-17 09:12:25 +0200

    Done 17 - Create extraction zone

--- Source/FPSGame/Private/FPSExtractionZone.cpp
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -8,7 +8,7 @@
 AFPSExtractionZone::AFPSExtractionZone()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	//PrimaryActorTick.bCanEverTick = true;
 
 	OverlapComp = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapComp"));
 	OverlapComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
@@ -17,20 +17,27 @@ AFPSExtractionZone::AFPSExtractionZone()
 	OverlapComp->SetBoxExtent(FVector(200.0f));
 	RootComponent = OverlapComp;
 
-	OverlapComp->OnComponentBeginOverlap.__Internal_AddDynamic(this, &AFPSExtractionZone::HandleOverlap);
+	OverlapComp->SetHiddenInGame(false);
+
+	OverlapComp->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::HandleOverlap);
 }
 
 // Called when the game starts or when spawned
-void AFPSExtractionZone::BeginPlay()
+//void AFPSExtractionZone::BeginPlay()
+//{
+//	Super::BeginPlay();
+//	
+//}
+
+void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	Super::BeginPlay();
-	
+	UE_LOG(LogTemp, Log, TEXT("OVERLAPPED WITH EXTRACTION ZONE"));
 }
 
 // Called every frame
-void AFPSExtractionZone::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
+//void AFPSExtractionZone::Tick(float DeltaTime)
+//{
+//	Super::Tick(DeltaTime);
+//
+//}
 
--- Source/FPSGame/Public/FPSExtractionZone.h
+++ Source/FPSGame/Public/FPSExtractionZone.h
@@ -19,15 +19,16 @@ public:
 
 protected:
 	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	//virtual void BeginPlay() override;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UBoxComponent* OverlapComp;
-
-	void HandleOverlap();
+	
+	UFUNCTION()
+	void HandleOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 public:	
 	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
+	//virtual void Tick(float DeltaTime) override;
+		
 };
